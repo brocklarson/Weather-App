380a155bc38597fd6826f3bf44aec24c5b4d28fd
brocklarson <brock.larson0725@gmail.com>
2022-12-10 22:36:12 -0700

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,55 +1,51 @@
-async function getCurrentWeather (location, units){
-    try{
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=${units}&appid=2909a74a92741182ac952a9d5596b341`;
-        const response = await fetch(url, {mode: `cors`});
-        const data = await response.json();
-        return data;
-    }catch(err){
-        console.log(err);
-    }
-};
-
-async function getForecastWeather (location, units){
-    try{
-        const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${location.lat}&lon=${location.lon}&units=${units}&cnt=40&appid=2909a74a92741182ac952a9d5596b341`;
-        const response = await fetch(url, {mode: `cors`})
-        const data = await response.json();
-        return data;
-    }catch(err){
-        console.log(err);
-    }
-};
-
-async function convertToLatLon(searchTerm){
-    try{
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=1&appid=2909a74a92741182ac952a9d5596b341`;
-        const response = await fetch(url, {mode: `cors`});
-        const data = await response.json();
-        return data[0];
-    }catch(err){
-        console.log(err);
-    }
-}
+import getData from './weather.js';
 
 function getDateTime(timezone, dt = null){
     //converts time given from API to time in the searched city
     if(!dt) dt = new Date().getTime() / 1000;
-    date = new Date((dt + timezone) * 1000);
+    const date = new Date((dt + timezone) * 1000);
+
     const datetime = date.toUTCString();
     const day = date.getUTCDay();
     const hours = date.getUTCHours();
     return {datetime, day, hours};
 }
 
-function processData(location, currentWeather, forecastWeather){
-    const date = getDateTime(currentWeather.timezone);
-    const todayFull = date.datetime;
-    const todayDay = date.day;
+function mostCommonCond(array){
+    if(array.length == 0) return null;
+    let modeMap = {};
+    let maxEl = array[0].weather[0].main, maxCount = 1;
+    for(let i = 0; i < array.length; i++){
+        let el = array[i].weather[0].main;
+        if(modeMap[el] == null) modeMap[el] = 1;
+        else modeMap[el]++;  
+        if(modeMap[el] > maxCount){
+            maxEl = el;
+            maxCount = modeMap[el];
+        }
+    }
+    return maxEl;
+}
 
-    /* CURRENT WEATHER */
-    console.log('*Current Weather*')
+function extractDayWeather(n, currentWeather, forecastWeather, todayWeekday){
+    const days = [`Sun`, 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let weekday;
+    const day = forecastWeather.list.filter(item => {
+        const wd = getDateTime(currentWeather.timezone, item.dt).day;
+        if((wd === todayWeekday + n) || (wd === todayWeekday - (7 - n))){
+            weekday = wd;
+            return true;
+        }
+    });
+    day.sort((a,b) => a.main.temp > b.main.temp ? -1: 1);
+    day[0].wd = days[weekday];
+    return day;
+};
+
+function getCurrentWeather(location, currentWeather, todayFullDate){
+    console.log('\n*Current Weather*')
     console.log(`Location:`, location.name + `, ` + location.country);
-    console.log(`Time:`, todayFull);
+    console.log(`Time:`, todayFullDate);
     console.log(`Temp:`, currentWeather.main.temp);
     console.log(`Min Temp:`, currentWeather.main.temp_min);
     console.log(`Max Temp:`, currentWeather.main.temp_max);
@@ -57,72 +53,41 @@ function processData(location, currentWeather, forecastWeather){
     console.log(`Humidity:`, currentWeather.main.humidity);
     console.log(`Condition:`, currentWeather.weather[0].main);
     console.log(`Wind Speed:`, currentWeather.wind.speed);
-    console.log(`Today:`);
+}
 
-    /* HOURLY FORECAST */
+function getForecastWeather(currentWeather, forecastWeather, todayWeekday){
+    const day1 = extractDayWeather(1, currentWeather, forecastWeather, todayWeekday);
+    const day2 = extractDayWeather(2, currentWeather, forecastWeather, todayWeekday);
+    const day3 = extractDayWeather(3, currentWeather, forecastWeather, todayWeekday);
+    
+    //Convert to day of week
+    console.log('\n*3-Day Forecast*')
+    console.log(day1[0].wd, `Cond:`, mostCommonCond(day1), '| Max -', day1[0].main.temp, `| Min -`, day1[day1.length - 1].main.temp);
+    console.log(day2[0].wd, `Cond:`, mostCommonCond(day2), '| Max -', day2[0].main.temp, `| Min -`, day2[day2.length - 1].main.temp);
+    console.log(day3[0].wd, `Cond:`,  mostCommonCond(day3), '| Max -', day3[0].main.temp, `| Min -`, day3[day3.length - 1].main.temp);
+    //Better to just switch to most undesirable condition? Snow, Rain, Cloud, Clear ? Look up list of conditions on openweather
+}
+
+function getHourlyForecast(currentWeather, forecastWeather){
     console.log('\n*Hourly Forecast*');
     forecastWeather.list.forEach(item =>{
         const dateTime = getDateTime(currentWeather.timezone, item.dt);
         console.log(`Forecast: Day -`, dateTime.day, `| Time -`, dateTime.hours, `| Temp -`, item.main.temp);
     });
-
-
-    /* 3-DAY FORECAST */
-    /* CLEAN THIS SECTION UP! */
-    console.log('\n*3-Day Forecast*')
-    function mostCommonCond(array){
-        if(array.length == 0) return null;
-        var modeMap = {};
-        var maxEl = array[0].weather[0].main, maxCount = 1;
-        for(var i = 0; i < array.length; i++){
-            var el = array[i].weather[0].main;
-            if(modeMap[el] == null) modeMap[el] = 1;
-            else modeMap[el]++;  
-            if(modeMap[el] > maxCount){
-                maxEl = el;
-                maxCount = modeMap[el];
-            }
-        }
-        return maxEl;
-    }
-
-    const day1 = forecastWeather.list.filter(item => (getDateTime(currentWeather.timezone, item.dt).day === todayDay + 1 || getDateTime(currentWeather.timezone, item.dt).day === todayDay - 6));
-    day1.sort((a,b) => a.main.temp > b.main.temp ? -1: 1);
-    day1.forEach(period => {
-        const dateTime = getDateTime(currentWeather.timezone, period.dt);
-    }); 
-
-    const day2 = forecastWeather.list.filter(item => (getDateTime(currentWeather.timezone, item.dt).day === todayDay + 2 || getDateTime(currentWeather.timezone, item.dt).day === todayDay - 5));
-    day2.sort((a,b) => a.main.temp > b.main.temp ? -1: 1);
-    day2.forEach(period => {
-        const dateTime = getDateTime(currentWeather.timezone, period.dt);
-    });
-    const day3 = forecastWeather.list.filter(item => (getDateTime(currentWeather.timezone, item.dt).day === todayDay + 3 || getDateTime(currentWeather.timezone, item.dt).day === todayDay - 4));
-    day3.sort((a,b) => a.main.temp > b.main.temp ? -1: 1);
-    day3.forEach(period => {
-        const dateTime = getDateTime(currentWeather.timezone, period.dt);
-    });
-    
-    //Convert this to day of week
-    console.log('Day 1: Cond -', mostCommonCond(day1), '| Max -', day1[0].main.temp, "| Min -", day1[day1.length - 1].main.temp);
-    console.log('Day 2: Cond -', mostCommonCond(day2), '| Max -', day2[0].main.temp, "| Min -", day2[day2.length - 1].main.temp);
-    console.log('Day 3: Cond -', mostCommonCond(day3), '| Max -', day3[0].main.temp, "| Min -", day3[day3.length - 1].main.temp);
-    //Better to just switch to most undesirable condition? Snow, Rain, Cloud, Clear ? Look up list of conditions on openweather
-
 }
 
-async function getData(searchTerm = `Tokyo`, units = `Metric`){
-    try{
-        const location = await convertToLatLon(searchTerm);
-        const currentWeather = await getCurrentWeather(location, units);
-        const forecastWeather = await getForecastWeather(location, units);
-        processData(location, currentWeather, forecastWeather);
-    }catch(err){
-        console.log(err);
-    }
-};
+function processData(location, currentWeather, forecastWeather){
+    const date = getDateTime(currentWeather.timezone);
+    const todayFullDate = date.datetime;
+    const todayWeekday = date.day;
 
-getData();
+    getCurrentWeather(location, currentWeather, todayFullDate);    
+    getForecastWeather(currentWeather, forecastWeather, todayWeekday);
+    getHourlyForecast(currentWeather, forecastWeather);   
+}
 
-
-
+async function init(){
+    const data = await getData();
+    processData(data.location, data.currentWeather, data.forecastWeather);
+}
+init();
---
+++ src/weather.js
@@ -0,0 +1,45 @@
+async function getCurrentWeather (location, units){
+    try{
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=${units}&appid=2909a74a92741182ac952a9d5596b341`;
+        const response = await fetch(url, {mode: `cors`});
+        const data = await response.json();
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+};
+
+async function getForecastWeather (location, units){
+    try{
+        const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${location.lat}&lon=${location.lon}&units=${units}&cnt=40&appid=2909a74a92741182ac952a9d5596b341`;
+        const response = await fetch(url, {mode: `cors`})
+        const data = await response.json();
+        return data;
+    }catch(err){
+        console.log(err);
+    }
+};
+
+async function convertToLatLon(searchTerm){
+    try{
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=1&appid=2909a74a92741182ac952a9d5596b341`;
+        const response = await fetch(url, {mode: `cors`});
+        const data = await response.json();
+        return data[0];
+    }catch(err){
+        console.log(err);
+    }
+}
+
+async function getData(searchTerm = `Tokyo`, units = `Metric`){
+    try{
+        const location = await convertToLatLon(searchTerm);
+        const currentWeather = await getCurrentWeather(location, units);
+        const forecastWeather = await getForecastWeather(location, units);
+        return {location, currentWeather, forecastWeather};
+    }catch(err){
+        console.log(err);
+    }
+};
+
+export default getData;
